e880ca59cb5e5cf3bf582ece3796b1b80bf7b749
EISHU92 <donatto555@gmail.com>
2024-03-09 19:09:05 -0500

    Add On New Player ACT

--- cubiq/ehs_on_join/init.lua
+++ cubiq/ehs_on_join/init.lua
@@ -1,10 +1,11 @@
 --minetest.register_on_prejoinplayer(function(name, ip)
 --	core.chat_send_all(name.." esta entrando / "..name.." is joining the game")
 --end)
---[[
+
 minetest.register_on_newplayer(function(ObjectRef)
 	core.chat_send_all(ObjectRef:get_player_name().." se ha unido por primera ves! Di hola / "..ObjectRef:get_player_name().." has joined the server for the first time, say hi!")
 end)
+--[[
 minetest.register_on_authplayer(function(name, ip, is_success)
 	if is_success and ip then
 		core.chat_send_all("*** "..name.." is joining the game")
