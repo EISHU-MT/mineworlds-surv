d910d050881c9eb2615e7ef49889601aa14d659b
EISHU92 <donatto555@gmail.com>
2024-03-10 18:05:18 -0400

    ReFix Random money func

--- cubiq/emoney/random.lua
+++ cubiq/emoney/random.lua
@@ -19,9 +19,9 @@ core.register_globalstep(function(dtime)
 		local amount = f(min, max)
 		local div = amount / get_count()
 		for _, p in pairs(core.get_connected_players()) do
-			bank.add_value(Name(p), div)
+			bank.add_value(Name(p), math.floor(div))
 		end
-		core.chat_send_all(C("#49A2EA", "[Bank] "..bank.S("Server dropped @1 on every players!", tostring(div))))
+		core.chat_send_all(core.colorize("#49A2EA", "[Bank] "..bank.S("Server dropped @1$ on every players!", tostring(math.floor(div)))))
 		timer = 0
 	end
 end)
